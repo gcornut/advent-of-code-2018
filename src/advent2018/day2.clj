b764d7d79d3fe73e36070fceddfefe44d2f401cd
gcornut <gcornut@gmail.com>
2018-12-02 12:47:01 +0100

    Day2 part2

--- src/advent2018/day2.clj
+++ src/advent2018/day2.clj
@@ -1,5 +1,6 @@
 (ns advent2018.day2
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [clojure.data :as data]))
 
 
 (def input
@@ -11,12 +12,10 @@
 (defn calc-checksum [ids]
   (->>
     ids
-    (map
-      (comp
-        (juxt
-          #(some (comp #{2} val) %)
-          #(some (comp #{3} val) %))
-        frequencies))
+    (map (comp
+           (juxt #(some (comp #{2} val) %)
+                 #(some (comp #{3} val) %))
+           frequencies))
     (reduce (fn [acc [has-two? has-three?]]
               (-> acc
                   (update-in [0] + (if has-two? 1 0))
@@ -26,4 +25,29 @@
 
 (def output
   (calc-checksum input))
+
+; part two
+
+(defn has-one-difference? [coll]
+  (= 1 (count (filter (complement nil?) coll))))
+
+(defn find-similar-box
+  "Given a box id, find in coll of box ids another box with only one character difference"
+  [sbox boxes]
+  (reduce (fn [_ box]
+            (let [[in-a in-b in-both] (data/diff (seq sbox) (seq box))]
+              (when (and (has-one-difference? in-a)
+                         (has-one-difference? in-b))
+                (reduced in-both))))
+          boxes))
+
+(defn find-similar-boxes [boxes]
+  (some #(find-similar-box % boxes) boxes))
+
+(defn common-characters [differences]
+  (apply str differences))
+
+(def output2
+  (common-characters
+   (find-similar-boxes input)))
 
